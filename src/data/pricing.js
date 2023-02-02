a4898e2059fb935766fa46cf309d8fd5ae8a7dda
gitarthakalita <gitarthakalita.prsnl@gmail.com>
2023-02-02 20:44:58 +0530

    first commit

---
+++ src/data/pricing.js
@@ -0,0 +1,69 @@
+export const pricingData = [
+    {
+        title: "Basic",
+        description:"",
+        includes: [
+            {dashboardCount:1},  
+            "1 Users"
+        ],
+        priceAmount: "Free",
+        pricingImageUrl: "",
+        features: [
+            "Core data sources",
+            "sharing links"
+        ]
+    },
+    {
+        title: "Essential",
+        description: "Easily build and share dashboards that display essential business data.",
+        includes:[
+            {dashboardCount:1}, 
+            "3 Users", 
+            "Send to 1 TV"
+        ],
+        priceAmount: "3920.00",
+        pricingImageUrl: "",
+        features: [
+            "Everything in Basic",
+            "Dashboard Loops",
+            "Support via Email, chat and documentation"
+        ]
+    },
+    {
+        title: "Professional",
+        description: "Connect to over 80 data sources and keep teams in the loop with advanced sharing options.",
+        includes:[
+            {dashboardCount:1}, 
+            "10 Users", 
+            "Send to 3 TV"
+        ],
+        priceAmount: "7920.00",
+        pricingImageUrl: "",
+        features: [
+            "Everything in Essential, plus...",
+            "Pro data sources",
+            "Send to Slack",
+            "Send dashboard via Email",
+            "Use your own logo",
+            "Priority Support"
+        ]
+    },
+    {
+        title: "Scale",
+        description: "Introduce KPI dashboards into multiple areas of your business.",
+        includes:[
+            {dashboardCount:30}, 
+            "25 Users", 
+            "Send to 10 TV"
+        ],
+        priceAmount: "55,920.00",
+        pricingImageUrl: "",
+        features: [
+            "Everything in Pro, plus...",
+            "Single Sign On (SSO)",
+            "Pay by bank transfer or PO",
+            "Advanced dashboard theming",
+            "Dedicated support rep"
+        ]
+    }
+]
